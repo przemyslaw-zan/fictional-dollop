42479b76b80e66435092d7e22ce168eceb4b6064
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-04-16 16:13:21 +0200

    Foo

---
+++ changelog.js
@@ -0,0 +1,10 @@
+/**
+ * @license Copyright (c) 2022-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import { generateChangelogForSinglePackage } from '@ckeditor/ckeditor5-dev-release-tools';
+
+generateChangelogForSinglePackage( {
+	skipLinks: true
+} );
